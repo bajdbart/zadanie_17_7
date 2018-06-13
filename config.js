7d72c51b3384dcce6e9fce3d02a04195e46f5673
Bartłomiej Bajda <allerbbb@gmail.com>
2018-06-13 15:27:47 +0200

    add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '863047955429-tbia8jkvu5nnf8j9pi25r4erfjpiv99p.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'lKt32-GU8NYrouJ9l1WovItw',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
